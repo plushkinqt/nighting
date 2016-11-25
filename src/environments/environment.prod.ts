d116ecc3a0ec8c4d3e18cadad82cc881f93960e5
Riku Kallio <riku.kallio@zef.fi>
2016-11-25 18:40:12 +0200

    Updated clientid and secret

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+
+  clientId: "KLDWEQPSBHAPSBVZ",
+
+  clientSecret: "3NMVITI6O6JFVFEBGAJIU7N6TTSGNLSZ"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,9 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  clientId: "KLDWEQPSBHAPSBVZ",
+
+  clientSecret: "3NMVITI6O6JFVFEBGAJIU7N6TTSGNLSZ"
 };
