7aad92403af781fa5db3d1bd6c4de1808e3ddc55
Riku Kallio <riku.kallio@zef.fi>
2016-11-25 20:50:27 +0200

    Fix another merge conflict

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
-<<<<<<< Updated upstream
-import {Component, OnInit} from '@angular/core';
-import {OAuthService} from "angular2-oauth2/oauth-service";
-import {environment} from "../environments/environment";
-import {URLSearchParams, Http} from "@angular/http";
-import { AuthService } from "./services/auth.service";
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { OAuthService } from "angular2-oauth2/oauth-service";
-import { environment } from "../environments/environment";
 import { URLSearchParams, Http } from "@angular/http";
+
+import { OAuthService } from "angular2-oauth2/oauth-service";
+
+import { AuthService } from "./services/auth.service";
+import { environment } from "../environments/environment";
 
 @Component({
     selector: 'app-root',
