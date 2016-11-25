89306000374d859e75e348f1c0024cfac6d388fc
Georgi Yanev <ext-georgi.yanev@f-secure.com>
2016-11-25 19:29:10 +0200

    add scroller component to present matches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ScrollerComponent } from './scroller/scroller.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrollerComponent
   ],
   imports: [
     BrowserModule,
