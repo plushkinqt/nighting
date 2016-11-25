d7a628f1e9fb6550121f59797db5033601351b8a
mikedanylov <mike.danylov@gmail.com>
2016-11-25 19:32:24 +0200

    Initial settings for oauth2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OAuthService} from "angular2-oauth2/oauth-service";
+import {environment} from "../environments/environment";
+import {URLSearchParams, Http} from "@angular/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+    title = 'app works!';
+
+    constructor(private oauthService: OAuthService,
+                private http: Http) {}
+
+    ngOnInit() {
+        // Login-Url
+        this.oauthService.loginUrl = "https://api.ouraring.com/oauth/authorize";
+
+        // URL of the SPA to redirect the user to after login
+        this.oauthService.redirectUri = window.location.origin;
+
+        // The SPA's id. Register SPA with this id at the auth-server
+        this.oauthService.clientId = environment.clientId;
+
+        // The name of the auth-server that has to be mentioned within the token
+        this.oauthService.issuer = "https://api.ouraring.com";
+
+        // set to true, to receive also an id_token via OpenId Connect (OIDC) in addition to the
+        // OAuth2-based access_token
+        this.oauthService.oidc = true;
+
+        // Use setStorage to use sessionStorage or another implementation of the TS-type Storage
+        // instead of localStorage
+        this.oauthService.setStorage(sessionStorage);
+
+        // To also enable single-sign-out set the url for your auth-server's logout-endpoint here
+        this.oauthService.logoutUrl = "https://api.ouraring.com/oauth/authorize";
+
+        // This method just tries to parse the token within the url when
+        // the auth-server redirects the user back to the web-app
+        // It dosn't initiate the login
+        this.oauthService.tryLogin({});
+    }
+
+    public login() {
+        this.oauthService.initImplicitFlow();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { OAuthService } from 'angular2-oauth2/oauth-service';
 
 import { AppComponent } from './app.component';
 
@@ -16,7 +17,8 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [
-    AuthService
+      AuthService,
+      OAuthService
   ],
   bootstrap: [AppComponent]
 })
