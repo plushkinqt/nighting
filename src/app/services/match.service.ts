2d8d970966170912fc2e4dbcb27e45507bd6d6ff
plushkinqt <plushkinqt@gmail.com>
2016-11-26 18:30:01 +0200

    Added couple of values

--- src/app/services/match.service.ts
+++ src/app/services/match.service.ts
@@ -9,8 +9,18 @@ export class MatchService {
     console.log(userData1, userData2);
 
     let sleepTotals = 0;
-    let agvScore1 = 0;
-    let agvScore2 = 0;
+    let user1 = {
+      agvScore: 0,
+      avgAwake: 0,
+      rem: 0,
+      deep: 0
+    };
+    let user2 = {
+      agvScore: 0,
+      avgAwake: 0,
+      rem: 0,
+      deep: 0
+    };
 
     userData1.sleep.forEach((sleep, index) => {
       let diffpercent = this.compare(sleep.duration, userData2.sleep[index].duration);
@@ -20,17 +30,42 @@ export class MatchService {
 
       sleepTotals += (diffpercent + awakepercent + effpercent + scorepercent) / 4;
 
-      agvScore1 += sleep.score;
-      agvScore2 += userData2.sleep[index].score;
+      user1.agvScore += sleep.score;
+      user2.agvScore += userData2.sleep[index].score;
+
+      user1.avgAwake += sleep.awake;
+      if (userData2.sleep[index].awake) {
+        user2.avgAwake += userData2.sleep[index].awake;
+      }
+
+      user1.rem += sleep.rem;
+      if (userData2.sleep[index].rem) {
+        user2.rem += userData2.sleep[index].rem;
+      }
+
+      user1.deep += sleep.deep;
+      if (userData2.sleep[index].deep) {
+        user2.deep += userData2.sleep[index].deep;
+      }
     });
 
-    agvScore1 = agvScore1/(userData1.sleep.length);
-    agvScore2 = agvScore2/(userData2.sleep.length);
+    user1.agvScore = user1.agvScore / (userData1.sleep.length);
+    user2.agvScore = user2.agvScore / (userData2.sleep.length);
+
+    user1.avgAwake = user1.avgAwake / userData1.sleep.length;
+    user2.avgAwake = user2.avgAwake / userData2.sleep.length;
+
+    user1.rem = user1.rem / userData1.sleep.length;
+    user2.rem = user2.rem / userData2.sleep.length;
+
+    user1.deep = user1.deep / userData1.sleep.length;
+    user2.deep = user2.deep / userData2.sleep.length;
+
 
     return {
-      "agvScore1": agvScore1,
-      "agvScore2": agvScore2,
-      "match": sleepTotals/(userData1.sleep.length) * 100
+      'match': (sleepTotals / (userData1.sleep.length) * 100),
+      user1: user1,
+      user2: user2
     };
   }
 
@@ -40,7 +75,7 @@ export class MatchService {
     if (value1 == value2)
       return 1;
 
-    if (value1 == null || value2 == null)
+    if (value1 == null || value2 == null)
       return 0;
 
     return 1 - (value1 > value2 ? diff / value1 : diff / value2);
