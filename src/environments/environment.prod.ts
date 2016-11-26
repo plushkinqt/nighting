675be8d3e9c83aab13582e48c0c16ad8eb2c8d44
Riku Kallio <riku.kallio@zef.fi>
2016-11-26 11:19:22 +0200

    Added firebase config to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
 
+  firebase: {
+    apiKey: "AIzaSyAd5fJEOm-r1Q79tov-ike_6Ed9R0P1s6s",
+    authDomain: "nighting-f0356.firebaseapp.com",
+    databaseURL: "https://nighting-f0356.firebaseio.com",
+    storageBucket: "nighting-f0356.appspot.com",
+    messagingSenderId: "1076805095272"
+  },
+
   clientId: "KLDWEQPSBHAPSBVZ"
 };
